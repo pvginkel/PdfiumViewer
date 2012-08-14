1f904a2e9994c7b0681b55d4e0ad237c7fc42f44
Pieter van Ginkel <pvginkel@gmail.com>
2012-08-14 10:01:45 +0200

    Created version 0.3.0.0.

--- ChromePdfViewer/Properties/AssemblyInfo.cs
+++ ChromePdfViewer/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: Guid("ca63710b-8bc7-4fc5-8c39-210bacf591e8")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
