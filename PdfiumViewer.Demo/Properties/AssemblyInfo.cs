2188f053636d01b5caccab6243ffa123d0f011e2
Pieter van Ginkel <pvginkel@gmail.com>
2015-04-10 07:36:59 +0200

    Created version 1.6.1.0.

--- PdfiumViewer.Demo/Properties/AssemblyInfo.cs
+++ PdfiumViewer.Demo/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("975b4cd9-0202-4b28-80a6-faac73629152")]
 
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
