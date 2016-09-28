6a0dd20d7d183639ce2442868834bacbf3725702
Pieter van Ginkel <pvginkel@gmail.com>
2016-09-28 16:29:10 +0200

    Build new native DLL's using a different method.
    The build script at https://github.com/pvginkel/PdfiumViewer/wiki/Building-PDFium updated to simplify building the native DLL's. The result of this compilation is part of this commit.

--- Contrib/pdfiumviewer.cpp
+++ Contrib/pdfiumviewer.cpp
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "../../public/fpdfview.h"
-#include "../../v8/include/v8.h"
-#include "../../v8/include/libplatform/libplatform.h"
+#include "public/fpdfview.h"
+#include "v8/include/v8.h"
+#include "v8/include/libplatform/libplatform.h"
 
 extern "C"
 {
