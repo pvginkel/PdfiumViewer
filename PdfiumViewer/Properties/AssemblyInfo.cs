2593329b382c599724ff49d41020aa71b6f6a16b
Pieter van Ginkel <pvginkel@gmail.com>
2017-11-06 15:34:20 +0100

    Created version 2.13.0.0.

--- PdfiumViewer/Properties/AssemblyInfo.cs
+++ PdfiumViewer/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: Guid("ca63710b-8bc7-4fc5-8c39-210bacf591e8")]
 
-[assembly: AssemblyVersion("2.12.0.0")]
-[assembly: AssemblyFileVersion("2.12.0.0")]
+[assembly: AssemblyVersion("2.13.0.0")]
+[assembly: AssemblyFileVersion("2.13.0.0")]
